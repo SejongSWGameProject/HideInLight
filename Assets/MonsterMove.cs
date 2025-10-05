ab927b67617f6806fa3485ca8d85b1696e4a1834
gyu6172 <gyu6172@gmail.com>
2025-10-05 13:43:22 +0900

    NavMeshSurface, Agent추가
    괴물이 미로를 따라 플레이어에게 찾아간다.
    충돌처리가 잘 안되어 있어서 벽을 살짝 뚫는다.

--- Assets/MonsterMove.cs
+++ Assets/MonsterMove.cs
@@ -17,5 +17,13 @@ public class MonsterFollow : MonoBehaviour
         {
             agent.SetDestination(player.position); // 플레이어 위치 따라감
         }
+        if (agent.isOnNavMesh) // NavMesh 위에 있을 때만 실행
+        {
+            agent.SetDestination(player.position);
+        }
+        else
+        {
+            Debug.LogWarning("괴물이 NavMesh 위에 있지 않습니다!");
+        }
     }
 }
