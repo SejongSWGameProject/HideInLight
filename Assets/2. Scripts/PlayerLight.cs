df979b1be295bfc196f71ceb01b2a6873cd5f966
gyu6172 <gyu6172@gmail.com>
2025-10-30 15:18:05 +0900

    손전등 깜빡거림 해결

--- Assets/2. Scripts/PlayerLight.cs
+++ Assets/2. Scripts/PlayerLight.cs
@@ -5,9 +5,21 @@ public class FlashlightCtrl : MonoBehaviour
     public Light flashlight;          // Spot Light 연결
     public Transform cameraTransform; // 카메라 Transform
 
+    [SerializeField] private float flashlightAngular = 30F;
+    [SerializeField] private float flashlightRange = 20F;
+
     void Start()
     {
+        //flashlight.range = flashlightRange;
+        //flashlight.spotAngle = flashlightAngular;
         flashlight.enabled = false; // 시작할 때 손전등 끄기
+    }
+
+    private void LateUpdate()
+    {
+        // 카메라 위치와 회전에 맞춰 손전등 이동
+        flashlight.transform.position = cameraTransform.position;
+        flashlight.transform.rotation = cameraTransform.rotation;
     }
 
     void Update()
@@ -18,8 +30,6 @@ public class FlashlightCtrl : MonoBehaviour
             flashlight.enabled = !flashlight.enabled; // 켜져있으면 끄고, 꺼져있으면 켬
         }
 
-        // 카메라 위치와 회전에 맞춰 손전등 이동
-        flashlight.transform.position = cameraTransform.position;
-        flashlight.transform.rotation = cameraTransform.rotation;
+        
     }
 }
