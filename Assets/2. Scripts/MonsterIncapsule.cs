6760e28d55b7026d9a1e0d2c671dee305973ce57
gyu6172 <gyu6172@gmail.com>
2025-12-07 02:59:22 +0900

    캡슐 깨는 사운드 추가

---
+++ Assets/2. Scripts/MonsterIncapsule.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class MonsterIncapsule : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public AudioClip capsuleBreakClip;
+    private AudioSource audioSource;
+    public float threshold = 0.1f;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+
+    private void OnDisable()
+    {
+        if (capsuleBreakClip != null)
+        {
+            AudioSource.PlayClipAtPoint(capsuleBreakClip, transform.position, threshold);
+        }
+    }
+}
